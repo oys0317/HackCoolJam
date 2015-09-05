978be54ee531c946309cf8195cf8e0794134528c
g0c0b <younsukoh777@gmail.com>
2015-09-05 04:40:59 -0700

    barrel and frame prefab

---
+++ BubbleVolleyball/Assets/Scripts/BarrelMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BarrelMovement : MonoBehaviour {
+	public float speed = 1f;
+
+	Rigidbody2D barrelRB;
+
+	// Use this for initialization
+	void Start () {	
+		barrelRB = GetComponent<Rigidbody2D>();
+		barrelRB.velocity = speed * new Vector2 (0f, 1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
